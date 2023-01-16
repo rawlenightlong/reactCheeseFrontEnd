83ae23babc7d4eb496c27f443cc7d443436c7075
Rawle Becerril <rawlesxm@gmail.com>
2023-01-16 13:32:54 -0500

    updated routes

--- src/router.js
+++ src/router.js
@@ -3,7 +3,11 @@ import App from "./App"
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
-
+        <Route path="" element={<Index/>}/>
+        <Route path=":id" element={<Show/>}/>
+        <Route path="create"/>
+        <Route path="update/:id"/>
+        <Route path="delete/:id"/>
     </Route>
 ))
 
